58455df5ff9313ee4280055b793d826e4280adbe
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-09-28 22:14:55 +0200

    Partie POST terminée, debut de la partie PUT

--- src/actions/post.action.js
+++ src/actions/post.action.js
@@ -3,13 +3,22 @@ import axios from "axios";
 
 // Mes types d'action :
 export const GET_POSTS = "GET_POSTS";
+export const ADD_POST = "ADD_POST";
 
 // Mes actions :
 export const getPosts = () => {
 	return (dispatch) => {
 		return axios.get("http://localhost:3000/posts").then((res) => {
 			dispatch({ type: GET_POSTS, payload: res.data });
-		})
-	}
-}
+		});
+	};
+};
+
+export const addPost = (data) => {
+	return (dispatch) => {
+		return axios.post("http://localhost:3000/posts", data).then((res) => {
+			dispatch({ type: ADD_POST, payload: data });
+		});
+	};
+};
 
--- src/reducers/post.reducer.js
+++ src/reducers/post.reducer.js
@@ -4,6 +4,8 @@ export default function postReducer(state = initialState, action) {
 	switch (action.type) {
 		case "GET_POSTS":
 			return action.payload;
+		case "ADD_POST":
+			return [action.payload, ...state]; // On ajoute le contenu du payload au State (qu'on déstructure)
 		default:
 			return state;
 	}
