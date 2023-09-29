4c40fd0f5b55085c2540f0a8745ddd53c59acc2f
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-09-29 17:10:36 +0200

    Ajout des commentaires (en cours)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import PostForm from "./components/PostForm";
-import User from "./components/User";
-import Post from "./components/Post"
-import { isEmpty } from "./components/Utils";
+// Import des modules nécessaires
+import React from "react"; // Import de React
+import { useSelector } from "react-redux"; // Import du hook useSelector de Redux, pour accéder à l'état du store Redux
+import PostForm from "./components/PostForm"; // Import du composant pour le formulaire de post
+import User from "./components/User"; // Import du composant pour l'affichage de l'utilisateur
+import Post from "./components/Post" // Import du composant pour afficher un post
+import { isEmpty } from "./components/Utils"; // Import d'une fonction utilitaire pour vérifier si une variable est vide
 
 const App = () => {
-  // Le useSelector permet de récupérer des data dans le store
+  // On utilise le hook useSelector pour récupérer les posts depuis le state global de l'application.
+  // state.postReducer fait référence à la partie du state gérée par le réducteur "postReducer"
   const posts = useSelector((state) => state.postReducer);
-
 
   return (
     <div>
-      <h1>Extreme</h1>
-      <PostForm />
+      <h1>Extreme</h1> 
+      <PostForm /> {/* Affichage du formulaire de post */}
+
       <div className="content">
         <div className="post-container">
-          {/* // On vérifie si le tableau posts est vide ou non Pour ne pas mapper avant l'arrivée des données */}
+          {/* 
+            On vérifie si le tableau de posts n'est pas vide en utilisant la fonction isEmpty.
+            Si le tableau n'est pas vide, on utilise map pour parcourir chaque post et l'afficher 
+            à l'aide du composant Post. On donne également une clé à chaque élément pour aider React 
+            à identifier chaque élément lors de la mise à jour du DOM.
+          */}
           {!isEmpty(posts)
             && posts.map((post, index) => (
               <Post post={post} key={index} />
             ))}
         </div>
-        <User />
+        <User /> {/* Affichage du composant User pour montrer les détails de l'utilisateur */}
       </div>
     </div>
   );
 };
 
+// On exporte le composant App pour pouvoir l'utiliser ailleurs dans l'application
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./styles/index.scss";
+// On importe les modules nécessaires.
+import React from "react";                  // Import principal de React.
+import ReactDOM from "react-dom";           // React DOM pour la manipulation du DOM.
+import App from "./App";                    // Import de notre composant principal App.
+import "./styles/index.scss";              // Import des styles globaux pour l'application.
 
 // REDUX :
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers";
-import { getPosts } from "./actions/post.action";
-import { getUser } from "./actions/user.action";
+// Import des modules nécessaires pour la configuration de Redux.
+import { Provider } from "react-redux";    // Provider englobe notre application pour qu'elle puisse accéder au store Redux.
+import { configureStore } from "@reduxjs/toolkit"; // Pour configurer et créer notre store.
+import rootReducer from "./reducers";      // Import du reducer combiné (tous nos reducers fusionnés en un seul).
+import { getPosts } from "./actions/post.action"; // Import de l'action pour récupérer les posts.
+import { getUser } from "./actions/user.action"; // Import de l'action pour récupérer l'utilisateur.
 
+// Configuration du store Redux.
 const store = configureStore({
-	reducer: rootReducer,
-	devTools: true, // Enable Redux DevTools Extension (only in dev mode !)
+	reducer: rootReducer,               // On spécifie notre reducer combiné.
+	devTools: true,                     // Active les outils de développement Redux (à utiliser seulement en mode développement !).
 })
 
+// Lors de l'initialisation de l'application, on déclenche les actions pour récupérer les posts et les infos de l'utilisateur.
 store.dispatch(getPosts());
 store.dispatch(getUser());
 
+// On rend notre composant App à l'intérieur de l'élément avec l'ID 'root' dans le HTML.
+// Le composant Provider englobe App pour fournir le store Redux à toute notre application.
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
 	</Provider>
-
-	, document.getElementById("root"));
+	, document.getElementById("root")); 
